e2b3dbc12d9ff77b1031f9b809906f231e5fa31f
中村彦一郎 <hk.nakamura@gmail.com>
2025-10-14 14:46:00 +0900

    ランディングページとフッターの更新: もっと知るリンク先をconceptに変更、フッターにHome追加、Terms削除

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -42,7 +42,7 @@ const LandingPage: React.FC = () => {
                 アプリを試す
               </Link>
               <Link 
-                to="/about" 
+                to="/concept" 
                 className="border-2 border-white text-white px-8 py-4 rounded-lg text-lg font-semibold hover:bg-white hover:bg-opacity-30 hover:-translate-y-1 transition-all duration-200 bg-white bg-opacity-20"
               >
                 もっと知る
